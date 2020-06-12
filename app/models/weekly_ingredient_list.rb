ee272455b6550fedf75b48ab3f38da1cd6c64c25
Matthias Mau <matthias.mau@outlook.com>
2020-06-12 11:19:09 +0300

    layout update on week menu page -> to be reviewed later

--- app/models/weekly_ingredient_list.rb
+++ app/models/weekly_ingredient_list.rb
@@ -1,9 +1,10 @@
 class WeeklyIngredientList < ApplicationRecord
   has_many :weekly_ingredients, dependent: :destroy
   has_many :ingredients, through: :weekly_ingredients
+  has_many :recipes
 
   validates :date, :menu_name, :price_per_portion, presence: true
-  
+
   # def add_ingredient(ingredient, amount)
   #   WeeklyIngredient.create(ingredient: ingredient, amount: amount, weekly_ingredient_list: self)
   # end
