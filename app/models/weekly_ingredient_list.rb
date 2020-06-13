4c413efa4be059c1546fc4142ed6f860768e3c44
Matthias Mau <matthias.mau@outlook.com>
2020-06-13 08:22:55 +0300

    updates to week menu and (all) recipe pages, both logic and styling

--- app/models/weekly_ingredient_list.rb
+++ app/models/weekly_ingredient_list.rb
@@ -8,4 +8,14 @@ class WeeklyIngredientList < ApplicationRecord
   # def add_ingredient(ingredient, amount)
   #   WeeklyIngredient.create(ingredient: ingredient, amount: amount, weekly_ingredient_list: self)
   # end
+
+  def created_this_week?
+    date = Date.today
+    start_week = date.at_beginning_of_week
+    end_week = start_week + 6.days
+    # date_beginning_this_week = Date.today.beginning_of_week
+    # date_today= Date.today
+    WeeklyIngredientList.where(:created_at => start_week..end_week)
+  end
+
 end
