ca9d50f16b2c465f8feb54181bb0506bc5eecb38
Matthias Mau <matthias.mau@outlook.com>
2020-06-14 07:42:41 +0300

    updated user profile page to include order history and user's own recipes. also added the user edit functionality so that user can update their profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,37 @@
 class UsersController < ApplicationController
-    def show
-        @user = User.find(params[:id])
-        @recipes_user = Recipe.where(user: current_user)
-        authorize @user
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
+    set_user
+    @recipes_user = Recipe.where(user: current_user)
+    @orders_user = Order.where(user: current_user)
+    # @order = Order.find(params[:order_id])
+    # @weekly_ingredient_list = @orders_user.weekly_ingredient_list
+  end
+
+  def edit
+    set_user
+  end
+
+  def update
+    set_user
+    @user.update(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'new'
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :email, :photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+    authorize @user
+  end
+
 end
