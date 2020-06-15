fff3e741ec203febe4eeda9768aeaabf3f1e9efc
Matthias Mau <matthias.mau@outlook.com>
2020-06-15 13:41:39 +0300

    made filter to work with browser refresh, not work with reset or page redirect

--- app/javascript/packs/recipe_filter.js
+++ app/javascript/packs/recipe_filter.js
@@ -3,7 +3,8 @@ import Rails from "@rails/ujs";
 console.log('Test: Hello from the recipe filter JS');
 
 var checkBoxes = document.querySelectorAll(".form-check-input");
-var form = document.querySelector('form');
+
+var form = document.getElementById("filter-recipes");
 
 const categorisationFilter = () => {
   for (const check of checkBoxes) {
