8d4ad2c134cfecac6c70bb2c0053b109acdc690e
Matthias Mau <matthias.mau@outlook.com>
2020-06-16 17:12:49 +0300

    saving progress. total order calculation not working yet

--- app/javascript/packs/order-total.js
+++ app/javascript/packs/order-total.js
@@ -1,20 +1,22 @@
 console.log("Hello from order-total.js")
 
 const orderTotalCalculation = () => {
-  let pricePerPortion = document.getElementById('price_per_portion');
+  let pricePerPortion = document.getElementById('price_per_portion').dataset;
   console.log("Price per Portion: ", pricePerPortion)
 
   let portionAmount = document.getElementById('order_amount_portion');
   console.log("Amount of portion: ", portionAmount)
 
+  let orderTotal = document.getElementById('order-total-price');
+  console.log("Previuos Order total: ", orderTotal)
+
   portionAmount.addEventListener('change', (e) => {
     let amount = Number.parseInt(portionAmount.value)
-    let price = Number.parseInt(portionAmount.value)
-    let orderTotal = document.getElementById('order-total-price');
-    console.log("Previuos Order total: ", orderTotal)
+    let price = Number.parseInt(pricePerPortion.value)
+    let orderTotalValue = Number.parseInt(orderTotal.value)
 
-    orderTotal.value = amount * price
-    console.log("Order total after amount change: ", Number.parseInt(orderTotal))
+    orderTotalValue = amount * price
+    console.log("Order total after amount change: ", orderTotalValue)
   });
 
 }
